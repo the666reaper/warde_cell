579ab59e6e34c965b110c1f99ece82bc67fcd560
Rea.code <88689500+the666reaper@users.noreply.github.com>
2021-11-21 14:47:27 +0200

    v0.0.2.1

--- lib/Controller/itemes_list.dart
+++ lib/Controller/itemes_list.dart
@@ -1,6 +1,3 @@
 List<Map> test = [
-  {'image url': 'https://www.bing.com/images/search?view=detailV2&ccid=g6ThmWBT&id=C96FBA56574AA89C4DB5CBADC23C10133415DC8B&thid=OIP.g6ThmWBTWsIjSqntwURhIgHaFj&mediaurl=https%3a%2f%2fpixy.org%2fsrc%2f19%2f193722.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.83a4e19960535ac2234aa9edc1446122%3frik%3di9wVNBMQPMKtyw%26pid%3dImgRaw%26r%3d0&exph=1728&expw=2304&q=image&simid=608023440635287816&FORM=IRPRST&ck=9321CAFF1B9559EF8E8E9E2BF9B7D608&selectedIndex=8',
-   'productName': 'honor',
-   'in stock': 1
-  },
+  
 ];
